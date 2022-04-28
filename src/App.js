5b649d49ec1589f796b56adc7fde49c0af231765
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2022-04-28 18:07:39 -0500

    cloned the rest of scaffolding from tutorial project for easy styling, made necessary version changes like changing Switch to Routes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -9,9 +15,14 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path='/' exact element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Routes>
+        <Footer />
       </Router>
+      
     </>
   );
 }
